327abeebe7ab5ced04841bcad9f49c0101e7775b
Vishal Ramalingam <vishal.ramalingam@outlook.com.au>
2022-06-03 22:45:00 +1000

    added create user

---
+++ app/user/views.py
@@ -0,0 +1,8 @@
+"""Views for user API"""
+
+from rest_framework import generics
+from user.serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    """Create new user in the system"""
+    serializer_class = UserSerializer
